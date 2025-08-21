087bd0a696918ba9d36d67b11eab03ae753c8937
Phantom Coder <abdulahad474783@gmail.com>
2025-08-21 14:24:11 +0600

    With History

--- Temperature Scale.c
+++ Temperature Scale.c
@@ -53,7 +53,7 @@ int main()
     int n,m,c[1000];
     char in,out;
     float given[1000],res[1000];
-    char sres[5][15]={"Celsius","Fahrenheit","Kelvin","Rankine"};
+    char sres[5][15]= {"Celsius","Fahrenheit","Kelvin","Rankine"};
     for (int i=0; ; i++)
     {
         printf("Enter 1 for continue and 0 for terminate: ");
@@ -68,138 +68,138 @@ int main()
             given[i]=temp;
             printf("Enter the first letter of the temperature you want to convert: ");
             scanf(" %c",&out);
-            if (in==out && (in=='c' || in=='C' || in=='f' || in=='F' || in=='k' || in=='K' || in=='r' || in=='R'))
+            if (in=='c' || in=='C')
             {
-                printf("The result: %.2f\n",temp);
-                res[i]=temp;
-                if (in=='c' || in=='C')
+                if (out=='c' || out=='C')
                 {
-                    c[i]=13;
+                    printf("The result: %.2f\n",temp);
+                    res[i]=temp;
+                    c[i]=1;
                 }
-                else if (in=='f' || in=='F')
+                else if (out=='f' || out=='F')
                 {
-                    c[i]=14;
+                    printf("The result: %.2f\n",convert1(temp));
+                    res[i]=convert1(temp);
+                    c[i]=2;
                 }
-                else if (in=='k' || in=='K')
+                else if (out=='k' || out=='K')
                 {
-                    c[i]=15;
+                    printf("The result: %.2f\n",convert3(temp));
+                    res[i]=convert3(temp);
+                    c[i]=3;
                 }
-                else if (in=='r' || in=='R')
+                else if (out=='r' || out=='R')
                 {
-                    c[i]=16;
-                }
-            }
-            else
-            {
-                if (in=='c' || in=='C')
-                {
-                    if (out=='f' || out=='F')
-                    {
-                        printf("The result: %.2f\n",convert1(temp));
-                        res[i]=convert1(temp);
-                        c[i]=1;
-                    }
-                    else if (out=='k' || out=='K')
-                    {
-                        printf("The result: %.2f\n",convert3(temp));
-                        res[i]=convert3(temp);
-                        c[i]=2;
-                    }
-                    else if (out=='r' || out=='R')
-                    {
-                        printf("The result: %.2f\n",convert7(temp));
-                        res[i]=convert7(temp);
-                        c[i]=3;
-                    }
-                    else
-                    {
-                        printf("Wrong entry\n");
-                        c[i]=0;
-                    }
-                }
-                else if (in=='f' || in=='F')
-                {
-                    if (out=='c' || out=='C')
-                    {
-                        printf("The result: %.2f\n",convert2(temp));
-                        res[i]=convert2(temp);
-                        c[i]=4;
-                    }
-                    else if (out=='k' || out=='K')
-                    {
-                        printf("The result: %.2f\n",convert5(temp));
-                        res[i]=convert5(temp);
-                        c[i]=5;
-                    }
-                    else if (out=='r' || out=='R')
-                    {
-                        printf("The result: %.2f\n",convert9(temp));
-                        res[i]=convert9(temp);
-                        c[i]=6;
-                    }
-                    else
-                    {
-                        printf("Wrong entry\n");
-                        c[i]=0;
-                    }
-                }
-                else if (in=='k' || in=='K')
-                {
-                    if (out=='c' || out=='C')
-                    {
-                        printf("The result: %.2f\n",convert4(temp));
-                        res[i]=convert4(temp);
-                        c[i]=7;
-                    }
-                    else if (out=='f' || out=='F')
-                    {
-                        printf("The result: %.2f\n",convert6(temp));
-                        res[i]=convert6(temp);
-                        c[i]=8;
-                    }
-                    else if (out=='r' || out=='R')
-                    {
-                        printf("The result: %.2f\n",convert11(temp));
-                        res[i]=convert11(temp);
-                        c[i]=9;
-                    }
-                    else
-                    {
-                        printf("Wrong entry\n");
-                        c[i]=0;
-                    }
-                }
-                else if (in=='r' || in=='R')
-                {
-                    if (out=='c' || out=='C')
-                    {
-                        printf("The result: %.2f\n",convert8(temp));
-                        res[i]=convert8(temp);
-                        c[i]=10;
-                    }
-                    else if (out=='f' || out=='F')
-                    {
-                        printf("The result: %.2f\n",convert10(temp));
-                        res[i]=convert10(temp);
-                        c[i]=11;
-                    }
-                    else if (out=='k' || out=='K')
-                    {
-                        printf("The result: %.2f\n",convert12(temp));
-                        res[i]=convert12(temp);
-                        c[i]=12;
-                    }
-                    else
-                    {
-                        printf("Wrong entry\n");
-                        c[i]=0;
-                    }
+                    printf("The result: %.2f\n",convert7(temp));
+                    res[i]=convert7(temp);
+                    c[i]=4;
                 }
                 else
                 {
                     printf("Wrong entry\n");
                     c[i]=0;
                 }
+            }
+            else if (in=='f' || in=='F')
+            {
+                if (out=='f' || out=='F')
+                {
+                    printf("The result: %.2f\n",temp);
+                    res[i]=temp;
+                    c[i]=5;
+                }
+                else if (out=='c' || out=='C')
+                {
+                    printf("The result: %.2f\n",convert2(temp));
+                    res[i]=convert2(temp);
+                    c[i]=6;
+                }
+                else if (out=='k' || out=='K')
+                {
+                    printf("The result: %.2f\n",convert5(temp));
+                    res[i]=convert5(temp);
+                    c[i]=7;
+                }
+                else if (out=='r' || out=='R')
+                {
+                    printf("The result: %.2f\n",convert9(temp));
+                    res[i]=convert9(temp);
+                    c[i]=8;
+                }
+                else
+                {
+                    printf("Wrong entry\n");
+                    c[i]=0;
+                }
+            }
+            else if (in=='k' || in=='K')
+            {
+                if (out=='k' || out=='K')
+                {
+                    printf("The result: %.2f\n",temp);
+                    res[i]=temp;
+                    c[i]=9;
+                }
+                else if (out=='c' || out=='C')
+                {
+                    printf("The result: %.2f\n",convert4(temp));
+                    res[i]=convert4(temp);
+                    c[i]=10;
+                }
+                else if (out=='f' || out=='F')
+                {
+                    printf("The result: %.2f\n",convert6(temp));
+                    res[i]=convert6(temp);
+                    c[i]=11;
+                }
+                else if (out=='r' || out=='R')
+                {
+                    printf("The result: %.2f\n",convert11(temp));
+                    res[i]=convert11(temp);
+                    c[i]=12;
+                }
+                else
+                {
+                    printf("Wrong entry\n");
+                    c[i]=0;
+                }
+            }
+            else if (in=='r' || in=='R')
+            {
+                if (out=='r' || out=='R')
+                {
+                    printf("The result: %.2f\n",temp);
+                    res[i]=temp;
+                    c[i]=13;
+                }
+                else if (out=='c' || out=='C')
+                {
+                    printf("The result: %.2f\n",convert8(temp));
+                    res[i]=convert8(temp);
+                    c[i]=14;
+                }
+                else if (out=='f' || out=='F')
+                {
+                    printf("The result: %.2f\n",convert10(temp));
+                    res[i]=convert10(temp);
+                    c[i]=15;
+                }
+                else if (out=='k' || out=='K')
+                {
+                    printf("The result: %.2f\n",convert12(temp));
+                    res[i]=convert12(temp);
+                    c[i]=16;
+                }
+                else
+                {
+                    printf("Wrong entry\n");
+                    c[i]=0;
+                }
+            }
+            else
+            {
+                printf("Wrong entry\n");
+                c[i]=0;
             }
             printf("-----------------------------------------------------------------\n");
         }
@@ -221,37 +221,37 @@ int main()
     for (int i=0; i<m; i++)
     {
         if (c[i]==1)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[1]);
-        else if (c[i]==2)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[2]);
-        else if (c[i]==3)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[3]);
-        else if (c[i]==4)
-            printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[0]);
-        else if (c[i]==5)
-            printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[2]);
-        else if (c[i]==6)
-            printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[3]);
-        else if (c[i]==7)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[0]);
-        else if (c[i]==8)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[1]);
-        else if (c[i]==9)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[3]);
-        else if (c[i]==10)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[0]);
-        else if (c[i]==11)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[1]);
-        else if (c[i]==12)
-            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[2]);
-        else if (c[i]==13)
             printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[0]);
-        else if (c[i]==14)
+        else if (c[i]==2)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[1]);
+        else if (c[i]==3)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[2]);
+        else if (c[i]==4)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[0],res[i],sres[3]);
+        else if (c[i]==5)
             printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[1]);
-        else if (c[i]==15)
+        else if (c[i]==6)
+            printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[0]);
+        else if (c[i]==7)
+            printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[2]);
+        else if (c[i]==8)
+            printf("%.2f %s\t %.2f %s\n",given[i],sres[1],res[i],sres[3]);
+        else if (c[i]==9)
             printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[2]);
-        else if (c[i]==16)
+        else if (c[i]==10)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[0]);
+        else if (c[i]==11)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[1]);
+        else if (c[i]==12)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[2],res[i],sres[3]);
+        else if (c[i]==13)
             printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[3]);
+        else if (c[i]==14)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[0]);
+        else if (c[i]==15)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[1]);
+        else if (c[i]==16)
+            printf("%.2f %s\t\t %.2f %s\n",given[i],sres[3],res[i],sres[2]);
         else if (c[i]==0)
             printf("Entry was wrong\n");
     }
